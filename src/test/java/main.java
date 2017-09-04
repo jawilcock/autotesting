1ca9592cd9e792ac70b8a83ef218feda1fa3eaad
johnandrewwilcock <john_wilko@live.co.uk>
2017-09-04 14:16:17 +0000

    first commit

---
+++ src/test/java/main.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main {
+
+
+    WebDriver driver;
+    @Before
+    public void setup(){
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void testOver(){
+        driver.quit();
+    }
+
+    @Test
+    public void myTest(){
+        driver.navigate().to("https://twitch.tv/musseduch");
+    }
+
+}
